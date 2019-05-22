88f239b0e9da1e053f2469472a824430f3734f71
Xin Gao <Xin.Gao@yh.nackademin.se>
2019-05-22 11:42:56 +0200

    lägga login metode

--- src/main/java/com/Daddit/app/DadService.java
+++ src/main/java/com/Daddit/app/DadService.java
@@ -10,6 +10,8 @@ public class DadService {
 
     @Autowired
     private DadRepository dadRepo;
+    
+    private Dad loginDad;
 
     Optional<Dad> findDadById(long Id) {
         return dadRepo.findById(Id);
@@ -18,7 +20,7 @@ public class DadService {
     Optional<Dad> findDadByUsernameandPassword(String username, String password) {
         return dadRepo.findByUsernameAndPassword(username, password);
     }
-    
+
     List<Dad> findAllDads() {
         return dadRepo.findAll();
     }
@@ -28,13 +30,26 @@ public class DadService {
         if (!dadRepo.findByUsername(dad.getUsername()).isPresent()) {
             //add a dad
             dadRepo.save(dad);
-            
+
             //return true or false
             return dad;
-        }
-        else {
+        } else {
             return null;
         }
+    }
+
+    public String logInDad(String username, String password) {
+        Optional<Dad> dOptional = dadRepo.findByUsername(username);
+        if (dOptional.isPresent() && dOptional.filter(dad -> dad.getPassword().equals(password)).isPresent()) {
+            loginDad=dOptional.get();
+            if (dOptional.get().isModerator() == false) {               
+                return "user";
+            } else {
+                return "admin";
+            }
+        }
+        return "Log in fail.";
+
     }
 
 }
