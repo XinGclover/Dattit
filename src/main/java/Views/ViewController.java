b93fade1f3dc193651a16857eeb615b7ab499ada
Kristian Grundström <kristian.grundstrom@yh.nackademin.se>
2019-05-23 10:08:02 +0200

    - Listing of all posts implemented - Form for creating new Dad implementet. - Listing of all Dads also implemented.

---
+++ src/main/java/Views/ViewController.java
@@ -0,0 +1,13 @@
+package Views;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ViewController {
+
+    public String index() {
+        return "index";
+    }
+}
--- src/main/java/com/Daddit/app/DadditApplication.java
+++ src/main/java/com/Daddit/app/DadditApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DadditApplication {
@@ -35,6 +37,4 @@ public class DadditApplication {
             
         };
     }
-    
-
 }
