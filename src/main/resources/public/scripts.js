802c2d96557118c312d6e0411ac0912e70d12b92
Xin Gao <Xin.Gao@yh.nackademin.se>
2019-05-27 15:39:45 +0200

    session storage funkar

--- src/main/resources/public/scripts.js
+++ src/main/resources/public/scripts.js
@@ -141,10 +141,17 @@ function userLogin() {
         dataType: "json",
         success: function (data) {
             var moderator = Object.values(data)[2];
-            if (moderator == true)
-                console.log("admin");
-            else
-                console.log("dad");
+//            console.log(data);
+            sessionStorage.setItem("username", JSON.stringify(data.username));
+            sessionStorage.setItem("moderator", JSON.stringify(data.moderator));
+//            console.log(sessionStorage.getItem("username"));
+//            if (moderator == true) {
+//                console.log("admin")
+//
+//
+//
+//            } else
+//                console.log("dad");
         },
         error: function (responseTxt, statusTxt, errorThrown) {
             console.log(errorThrown);
@@ -152,20 +159,60 @@ function userLogin() {
     });
 }
 
-function searchPosts(){
+console.log(sessionStorage.getItem("username"));
+
+
+function searchPostsbyString() {
     event.preventDefault();
     var searchString = $("#form-control").val();
     $.ajax({
         url: '/post/search',
         type: 'POST',
-        data: {str : searchString},
+        data: {str: searchString},
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         dataType: "json",
         success: function (data) {
-            console.log(data);
+//            emptyForm();
+            buildForm(data);
+            console.log(sessionStorage.getItem("username"));
         },
         error: function (responseTxt, statusTxt, errorThrown) {
             console.log(errorThrown);
         },
     });
+}
+
+function buildForm(data) {
+    var main = document.getElementById("main");
+    for (var post in data) {
+        var postItem = document.createElement('div');
+        postItem.setAttribute('class', 'mb-3');
+        postItem.innerHTML = `<div class="row no-gutters">
+                        <div class="col-md-1 bg-light text-center">
+                            <a href=""> <i class="fas fa-chevron-up"></i></a> <br>
+                            ` + countVotes(data[post].votes) + `<br>
+                            <a href=""> <i class="fas fa-chevron-down"></i></a>
+                        </div>
+                        <div class="col-md-11">
+                            <div class="card-body">
+                                <p class="card-header bg-white pt-0">r/`
+                + printCategories(data[post].categories)
+                + ` • Posted by u/`
+                + data[post].dad.username + ` `
+                + data[post].created + `
+                    <h5 class="card-title">` + data[post].headline + `</h5>
+                            <p class="card-text">` + data[post].content + `
+                            </p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                    </div>
+                    </div>
+                        `;
+        main.appendChild(postItem);
+    }
+}
+
+function emptyForm() {
+    var main = document.getElementById("main");
+    main.innerHTML = "";
 }
